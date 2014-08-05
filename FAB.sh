8e001c474329f573dde81b6bd027671117d94731
Chris Barker <cbarker@puppetlabs.com>
2014-08-05 09:20:08 -0700

    Oscar environment used for documentation creation

---
+++ FAB.sh
@@ -0,0 +1,2 @@
+alias vFAB="fab -i /Users/cbarker/.ssh/insecure_private_key -u vagrant -n 3 -P -H 'ca.er.puppetlabs.demo,mcohub.er.puppetlabs.demo,mcospoke1.er.puppetlabs.demo,compile1.er.puppetlabs.demo,puppetdb.er.puppetlabs.demo' --"
+alias sFAB="fab -i /Users/cbarker/.ssh/insecure_private_key -u vagrant -n 3 -P -H"
